4ef9bc4b95fec0f5d50c9b403b16acbeb0d0cf33
raintor <2837714831@qq.com>
2019-09-07 21:33:49 +0800

    update treenode

---
+++ src/main/java/LeetCode精选面试题/TreeNode.java
@@ -0,0 +1,13 @@
+package LeetCode精选面试题;
+
+/**
+ * @author: raintor
+ * @Date: 2019/9/7 21:32
+ * @Description:
+ */
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+}
